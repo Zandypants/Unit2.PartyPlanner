bd617af7119ca1468ce9d184503425f0d039ba6a
Alexander Linse <zandervon24@gmail.com>
2024-03-19 15:42:33 -0500

    Init state and references

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("Link Test");
+// State
+// define some state to store event info
+const state = {
+  events: [],
+};
+
+// References
+const allEventsSection = document.querySelector("#allEvents");
+
+// fetch event info from API (asynchronously)
+  // parse to json
+  // store json data in state
+
+
+// render state to screen
+  // choose tag to use for each event (or split details from event into separate tags for styling purposes?)
+  // create DOM elements
+  // add state objects values to relevant tags
+  // add all DOM elements to the page at once
