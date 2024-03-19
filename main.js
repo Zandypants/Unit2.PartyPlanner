31a6b502d9861fe80b51779535b693538175f520
Alexander Linse <zandervon24@gmail.com>
2024-03-19 18:03:09 -0500

    Fetched events from API

--- main.js
+++ main.js
@@ -8,9 +8,20 @@ const state = {
 const allEventsSection = document.querySelector("#allEvents");
 
 // fetch event info from API (asynchronously)
-  // parse to json
-  // store json data in state
+async function getEvents () {
+  try {
+    const fetchedEvents = await fetch("https://fsa-crud-2aa9294fe819.herokuapp.com/api/2402-FTB-ET-WEB-FT/events");
+    // parse to json
+    const json = await fetchedEvents.json();
+    // store json data in state
+    state.events = json.data;
+    console.log(state.events);
+  } catch (error) {
+    console.log("Error caught:", error);
+  }
+}
 
+getEvents();
 
 // render state to screen
   // choose tag to use for each event (or split details from event into separate tags for styling purposes?)
